76c8c33213d5a8afa6098d8352a1455ce41f4dd0
Fabian Lozano <fabianlozano044@gmail.com>
2024-11-20 16:45:28 -0500

    Add Carousel component to WetlandDetail and enhance layout structure

--- src/app/dashboard/wetland/components/WetlandDetail.tsx
+++ src/app/dashboard/wetland/components/WetlandDetail.tsx
@@ -1,6 +1,8 @@
 import { MapPinIcon } from '@heroicons/react/24/solid';
 import  Map from '@/components/NewMap';
 import PHIndicator from './PHIndicator';
+import Carousel from '@/app/dashboard/wetland/components/Carousel';
+
 
 interface WetlandDetailProps {
     id: string;
@@ -27,19 +29,19 @@ interface WetlandDetailProps {
 const WetlandDetail: React.FC<WetlandDetailProps> = ({ id, name, location, status, nodes }) => {
     const sensors = Object.values(nodes).map(node => node.sensors);
     return (
-        <div className="flex w-full">
+        <div className="flex flex-col w-full gap-4">
             <div className='flex flex-col w-full rounded-md p-4 bg-white'>
                 <h2>{name}</h2>
                 <div className='flex gap-2'>
                     <MapPinIcon className="w-5 h-5 text-blue3" />
                     <p className='text-sm text-gray-500'>{location}</p>
                 </div>
-                <Map center={[51.505, -0.09]} zoom={-0.09}  />
+                <div className='h-60 w-full'>
+                    <Map center={[51.505, -0.09]} zoom={-0.09}  />
+                </div>
             </div>
-            <div className='grid grid-cols-1 gap-3 lg:gird-cols-3'>
-                {/* {sensors.map((sensor) => (
-                    
-                ))} */}
+            <div className='rounded-md p-4 h-60'>
+                <Carousel items={sensors} />
             </div>
         </div>
     );
